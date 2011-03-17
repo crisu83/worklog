01e0c260a52179fc4c9e24487d6229e70ed63cfd
jokelas <jokelas@localhost>
2011-03-17 22:38:33 +0200

    - Started working with the tag auto complete. - Added some ignores: runtime, idea project folderm, main config and assets.

--- protected/views/entry/_start.php
+++ protected/views/entry/_start.php
@@ -20,21 +20,20 @@
 		<?php $this->widget('zii.widgets.jui.CJuiAutoComplete',array(
 			'model'=>$model,
 			'attribute'=>'name',
-			'source'=>"js:
-function(request,response) {
-	$.ajax({
-		method: 'post',
-		url: '".Yii::app()->createUrl('assignment/juiAutoComplete')."',
-		data: { 
-			term: request.term,
-			project: $('#EntryStartForm_projectId-button .ui-selectmenu-status').text()
-		},
-		dataType: 'json',
-		success: function(data) {
-			response(data);
-		}
-	});
-}",
+			'source'=>"js:function(request,response) {
+				$.ajax({
+					method: 'post',
+					url: '".Yii::app()->createUrl('assignment/juiAutoComplete')."',
+					data: {
+						term: request.term,
+						project: $('#EntryStartForm_projectId-button .ui-selectmenu-status').text()
+					},
+					dataType: 'json',
+					success: function(data) {
+						response(data);
+					}
+				});
+			}",
 			'options'=>array(
 				'minLength'=>'2',
 			),
@@ -50,20 +49,82 @@ function(request,response) {
 	</div>
 	
 	<div class="row">
-		<?php echo $form->labelEx($model,'tags'); ?>
-		<?php $i=0; while( $i++<Yii::app()->params['tagFieldCount'] ): ?>
-			<?php $this->widget('zii.widgets.jui.CJuiAutoComplete',array(
-				'model'=>$model,
-				'attribute'=>'tags['.($i-1).']',
-				'source'=>Yii::app()->createUrl('tag/juiAutoComplete'),
-				'options'=>array(
-					'minLength'=>'2',
-				),
-				'htmlOptions'=>array(
-					'class'=>'tag-field',
-				),
-			)); ?>
-		<?php endwhile; ?>
+		<script type="text/javascript">
+			// tag/juiAutoComplete
+			$(document).ready(function() {
+
+				$.widget('ui.tagAutoComplete', {
+					_create: function() {
+
+						$(this.element).keydown(function(event) {
+							// Do not let the field blur if the auto complete list is open and were pushing TAB.
+							if( event.keyCode===$.ui.keyCode.TAB ) {
+								var menu = $(this).data('autocomplete').menu;
+								if( menu.element.is(':visible')===true ) {
+									if( menu.active===undefined ) {
+										console.log(menu.first());
+										console.log($(this).data('autocomplete'))
+									}
+
+									event.preventDefault();
+								}
+							}
+						});
+
+						$(this.element).autocomplete({
+							source: function(request, response) {
+								var realTerm = split(request.term).pop();
+
+								// Don't try to search with empty term.
+								if( !realTerm ) {
+									return false;
+								}
+
+								$.ajax({
+									url: '<?php echo Yii::app()->createUrl('tag/juiAutoComplete'); ?>',
+									data: { term: realTerm },
+									dataType: 'json',
+									success: function(data) {
+										response(data);
+									}
+								});
+							},
+							focus: function() {
+								// Prevent value inserted on focus.
+								return false;
+							},
+							select: function(event, ui) {
+								var terms = split( this.value );
+								// Remove the search term.
+								terms.pop();
+
+								// Add selected item.
+								terms.push(ui.item.value);
+
+								// Add placeholder to get the space at the end.
+								terms.push('');
+								this.value = terms.join(' ');
+								return false;
+							}
+						});
+
+						/**
+						 * Function that splits the given value with space.
+						 * @param val the value to split.
+						 */
+						function split(val) {
+							// Split with the separator.
+							return val.split(/ \s*/);
+						}
+					}
+				});
+
+				$('#EntryStartForm_tags').tagAutoComplete();
+			});
+		</script>
+
+		<?php echo $form->labelEx($model, 'tags'); ?>
+		<?php echo $form->textField($model, 'tags[]'); ?>
 	</div>
 
 	<div class="row buttons">
